4cf9ce4aab62fb2a7346f51c5787db94bf20220f
Nathan Hanzel <nathan.hanzel@gmail.com>
2023-07-27 12:08:22 -0600

    Added a home page and a projects page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundAnimationComponent } from './background-animation/background-animation.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundAnimationComponent
+    BackgroundAnimationComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
